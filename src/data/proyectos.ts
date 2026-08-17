fc06b027b4d9fa5f12381ccb0047c2e904487311
Masitas <nereoschmidt@gmail.com>
2026-08-16 22:40:45 -0300

    Agregando segundo proyecto (#3)

--- src/data/proyectos.ts
+++ src/data/proyectos.ts
@@ -16,9 +16,19 @@ export const misProyectos: Proyecto[] = [
         titulo: "Github Stats Pixel",
         descripcion:
             "Generador de imágenes estilo pixel art para estadísticas de GitHub. Creado para personalizar perfiles con una estética retro.",
-        tecnologias: ["React", "TypeScript", "Tailwind", "Next.js", "Vercel"],
+        tecnologias: ["React", "TypeScript", "TailwindCSS", "Next.js"],
         enlace: "https://github-estadisticas-pixel.vercel.app/api/stats/[TU_USUARIO]?type=general",
         repo: "https://github.com/MasitasIA/Github_Estadisticas_Pixel",
         imagen: "/GSP.png",
+    },
+    {
+        id: 2,
+        titulo: "Portfolio de Nicolas Tesio",
+        descripcion:
+            "Portfolio personal de Nicolas Tesio, desarrollado con React y TypeScript. Muestra proyectos, habilidades y experiencia profesional.",
+        tecnologias: ["React", "TypeScript", "TailwindCSS", "Vite", "Vercel"],
+        enlace: "https://mini-portafolio-nt.vercel.app",
+        repo: "https://github.com/MasitasIA/Mini-Portafolio-NT",
+        imagen: "/NT.png",
     },
 ];
